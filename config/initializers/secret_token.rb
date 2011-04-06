e64bfb84157acd2e00ea603ed4f2821bba886ed2
zhh <zhaohao.cn@gmail.com>
2011-04-06 23:30:06 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MBox::Application.config.secret_token = '3261670f9b6b7746e59f3a4fdeb97d7414b2e0076b1a2ac3f778a39fdf86d39b316a97c495b1a17be6a2af566fe34e6de6bc97a53fc975800fc99fb96e606692'
